15a949334cd1f20d34953960d7bfeaf6772a8566
StefanPetersTM <46031448+StefanPetersTM@users.noreply.github.com>
2019-04-03 21:38:28 +0200

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,24 +1,23 @@
 from tkinter import *
 from tkinter import filedialog
+
+
 window = Tk()
 window.title("Simple UI")
 window.geometry('250x100')
+
 lbl = Label(window, text="Hello World!")
-
 lbl.grid(column=0, row=0)
-
-
 
 
 def clicked():
     lbl.configure(text="The Button has been pressed")
-
 def exit():
     sys.exit(0)
-
 def loadfile():
         filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
 
+        
 btn1 = Button(window, text="Click Me", command=clicked)
 btn1.grid(column=0, row=1)
 
@@ -27,5 +26,6 @@ btn2.grid(column=0, row=2)
 
 button3 = Button(window, text=u"Choose a file", command=loadfile)
 button3.grid(column=0, row=3, sticky="ew", columnspan=1)
+
 
 window.mainloop()
