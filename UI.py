b8b7863bf39fa5c6b1b73053668f1d0447ca4067
StefanPetersTM <46031448+StefanPetersTM@users.noreply.github.com>
2019-04-10 21:31:31 +0200

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,20 +1,19 @@
 from tkinter import *
 from tkinter import filedialog
-#import ObjectRecognition
-#ObjectRecognition.somefunction()
+import ObjectRecognition.py
 
-
-#Creating the window
 window = Tk()
 window.title("Simple UI")
-window.geometry('250x150')
+window.geometry('250x100')
 lbl = Label(window, text="Hello World!")
 lbl.grid(column=0, row=0)
 
 
-#Definig all the functions
 def clicked():
-    lbl.configure(text="The Button has been pressed")
+    lbl.configure(text="Please select an image/video to be processed")
+
+    button1 = Button(window, text=u"Choose a file", command=loadfile)
+    button1.grid(column=0, row=1, sticky="ew", columnspan=1)
 
 def exit():
     sys.exit(0)
@@ -23,28 +22,21 @@ def loadfile():
         filename = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
         if filename:
             try:
-                self.settings["fileChosen"] = filename
+                filename = self.settings["fileChosen"]
 
             except:
                 print ("Selected file is %s" %filename)
                 return
 
-def start():
-        pass
 
-
-#Making the buttons
-btn1 = Button(window, text="Click Me", command=clicked)
+btn1 = Button(window, text="Click to start", command=clicked)
 btn1.grid(column=0, row=1)
 
 btn2 = Button(window, text="Click to exit", command=exit)
 btn2.grid(column=0, row=2)
 
-btn3 = Button(window, text=u"Choose a file", command=loadfile)
-btn3.grid(column=0, row=3, sticky="ew", columnspan=1)
-
-btn4 = Button(window, text=u"Start program.", command=start)
-btn4.grid(column=0, row=4, sticky="ew", columnspan=1)
 
 
 window.mainloop()
+
+
